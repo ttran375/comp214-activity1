9cc7ae1d1ffd3823224f7dcffdb4dd9002745f3c
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-14 23:30:44 +0000

    Update solutions

--- Workspace.sql
+++ Workspace.sql
@@ -68,7 +68,6 @@ BEGIN
 END;
 /
 
--- ## Question 3
 -- 3. Challenge 6-1: Create a function to return a description for the coffee grind. The input
 -- value 3 indicates “Whole Bean,” and the value 4 indicates “Ground.” If a NULL value is
 -- provided as input, the func/on should return N/A.
@@ -118,3 +117,52 @@ SELECT
     get_coffee_grind_description(bb_basketitem.option2) AS coffee_grind_description
 FROM
     bb_basketitem;
+
+-- 4. Challenge 6-2: Create a func/on to calculate the total pounds of coffee in an order. Keep
+-- in mind that the OPTION1 column in the BB_BASKETITEM table indicates the purchase of
+-- a half or whole pound. (This column is NULL for equipment items.) Also, you need to
+-- take the quan/ty of each item ordered into account. The input value to the func/on
+-- should be a basket ID.
+-- a. First, test the func/on with basket ID 3, using an anonymous block.
+-- b. Second, test the func/on in an SQL statement, using the BB_BASKET table, that
+-- shows every basket.
+CREATE OR REPLACE FUNCTION calculate_coffee_pounds(
+    basket_id IN NUMBER
+) RETURN NUMBER IS
+    total_pounds NUMBER := 0;
+BEGIN
+    SELECT
+        SUM(
+            CASE
+                WHEN bi.option1 = 1 THEN
+                    p.stock * 0.5
+                WHEN bi.option1 = 2 THEN
+                    p.stock
+                ELSE
+                    0
+            END * bi.quantity) INTO total_pounds
+    FROM
+        bb_basketitem bi
+        JOIN bb_product p
+        ON bi.idProduct = p.idProduct
+    WHERE
+        bi.idBasket = basket_id
+        AND p.Type = 'C';
+    RETURN total_pounds;
+END;
+/
+
+DECLARE
+    coffee_pounds NUMBER;
+BEGIN
+    coffee_pounds := calculate_coffee_pounds(3);
+    DBMS_OUTPUT.PUT_LINE('Total pounds of coffee in basket 3: '
+                         || coffee_pounds);
+END;
+/
+
+SELECT
+    idBasket,
+    calculate_coffee_pounds(idBasket) AS total_coffee_pounds
+FROM
+    bb_basket;
